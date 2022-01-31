76c7175ee419b94f3045b962015b21f9c6f3dc7a
Katemi Dev <60159484+Katemi-GitHub@users.noreply.github.com>
2022-01-31 21:34:52 +0000

    Create item_dict.py

---
+++ item_dict.py
@@ -0,0 +1,40 @@
+items = {
+    '1': {
+        'name':'wooden sword',
+        'damage': 5,
+        'defense': 0,
+        'type': 'weapon'
+    },
+    '2': {
+        'name':'bloody flesh',
+        'enemy': 'Zombie',
+        'damage': 0,
+        'defense': 0,
+        'drop_chance': 100,
+        'type': 'drop'
+    },
+    '3': {
+        'name':'Mossy Golem Hearth',
+        'enemy': 'Moss Golem',
+        'damage': 7,
+        'defense': 0,
+        'drop_chance': 25,
+        'type': 'drop'
+    },
+    '4': {
+        'name':'Greed Totem',
+        'enemy': 'Greed Totem',
+        'damage': 0,
+        'defense': 0,
+        'drop_chance': 100,
+        'type': 'drop'
+    },
+    '5': {
+        'name':'Expensive Slime',
+        'enemy': 'Greedy Slime',
+        'damage': 0,
+        'defense': 0,
+        'drop_chance': 75,
+        'type': 'drop'
+    }
+}
