30c07e72292bbf61ef98e166178adf83b1359c47
Katemi Dev <60159484+Katemi-GitHub@users.noreply.github.com>
2022-01-31 21:34:28 +0000

    Create enemy_data.py

---
+++ enemy_data.py
@@ -0,0 +1,42 @@
+types = {
+    '1': {
+        'Name': 'Zombie',
+        'Location': 'cobblestone',
+        'Rarity': 90,
+        'Health': 6,
+        'Damage': 3,
+        'Defense': 1
+    },
+    '2': {
+        'Name': 'Jungle Spider',
+        'Location': 'jungle',
+        'Rarity': 87,
+        'Health': 7,
+        'Damage': 5,
+        'Defense': 2
+    },
+    '3': {
+        'Name': 'Moss Golem',
+        'Location': 'moss',
+        'Rarity': 46,
+        'Health': 20,
+        'Damage': 6,
+        'Defense': 4
+    },
+    '4': {
+        'Name': 'Greedy Slime',
+        'Location': 'golden',
+        'Rarity': 70,
+        'Health': 13,
+        'Damage': 10,
+        'Defense': 3
+    },
+    '5': {
+        'Name': 'Greed Totem',
+        'Location': 'golden',
+        'Rarity': 15,
+        'Health': 50,
+        'Damage': 0,
+        'Defense': 5
+    }
+}
